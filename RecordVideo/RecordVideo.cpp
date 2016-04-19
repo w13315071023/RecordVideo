05692b17590970d511fc87f78eb3bf88b8e18f33
Mr.Sun <764607625@qq.com>
2016-04-19 14:56:15 +0800

    完成

--- RecordVideo/RecordVideo.cpp
+++ RecordVideo/RecordVideo.cpp
@@ -11,11 +11,11 @@
 #include "CameraImage.h"
 #include "CameraStatus.h"
 #include "SerialDll.h"
-#define LONTH 300
 using namespace std;
 
 INT	cameraNum = 2;
 tSdkCameraDevInfo	cameraInfo[2];
+int VideoLenth = 200;
 
 struct ImageData
 {
@@ -49,27 +49,55 @@ public:
 	}
 	void init(int index)
 	{
+		int ErrorID = 0;
 		if (0 == index)
 		{
-			CameraInit(&cameraInfo[0], -1, -1, &m_hCamera);
-			CameraSetCallbackFunction(m_hCamera, CameraCallBackOne, (PVOID)this, NULL);
-			
+			ErrorID = CameraInit(&cameraInfo[0], -1, -1, &m_hCamera);
+			if (!ErrorID)
+				printf("CameraInit\n");
+			else
+				printf("CameraInitError %d\n", ErrorID);
+			ErrorID = CameraSetCallbackFunction(m_hCamera, CameraCallBackOne, (PVOID)this, NULL);
+			if (!ErrorID)
+				printf("CameraSetCallbackFunction\n");
+			else
+				printf("CameraSetCallbackFunctionError %d\n", ErrorID);
 		}
 		else
 		{
-			CameraInit(&cameraInfo[1], -1, -1, &m_hCamera);
-			CameraSetCallbackFunction(m_hCamera, CameraCallBackTwo, (PVOID)this, NULL);
+			ErrorID = CameraInit(&cameraInfo[1], -1, -1, &m_hCamera);
+			if (!ErrorID)
+				printf("CameraInit\n");
+			else
+				printf("CameraInitError %d\n", ErrorID);
+			ErrorID = CameraSetCallbackFunction(m_hCamera, CameraCallBackTwo, (PVOID)this, NULL);
+			if (!ErrorID)
+				printf("CameraSetCallbackFunction\n");
+			else
+				printf("CameraSetCallbackFunctionError %d\n", ErrorID);
 		}
 
-		CameraGetCapability(m_hCamera, &m_sCameraInfo);
+		ErrorID = CameraGetCapability(m_hCamera, &m_sCameraInfo);
+		if (!ErrorID)
+			printf("CameraGetCapability\n");
+		else
+			printf("CameraGetCapabilityError %d\n", ErrorID);
 		FrameBufferRGB24 = CameraAlignMalloc(
 			m_sCameraInfo.sResolutionRange.iWidthMax*m_sCameraInfo.sResolutionRange.iHeightMax * 3,
 			16);
-		CameraSetAeState(m_hCamera, false);
-		CameraPlay(m_hCamera);
+		ErrorID = CameraSetAeState(m_hCamera, false);
+		if (!ErrorID)
+			printf("CameraSetAeState\n");
+		else
+			printf("CameraSetAeStateError %d\n", ErrorID);
+		ErrorID = CameraPlay(m_hCamera);
+		if (!ErrorID)
+			printf("CameraPlay\n");
+		else
+			printf("CameraPlayError %d\n", ErrorID);
 		printf("相机%d初始化完成\n", m_hCamera);
 
-		for (size_t i = 0; i < LONTH; i++)
+		for (size_t i = 0; i < VideoLenth; i++)
 		{
 			ImageData data;
 			data.pFrameBuffer = CameraAlignMalloc(
@@ -85,21 +113,30 @@ public:
 		{
 			swap(FrameBufferRGB24, m_Buffer[m_BufIdx].pFrameBuffer);
 			m_Buffer[m_BufIdx].pFrameHead = *pFrameHead;
-			m_BufIdx = m_BufIdx + 1 < LONTH ? m_BufIdx + 1 : 0;
+			m_BufIdx = m_BufIdx + 1 < VideoLenth ? m_BufIdx + 1 : 0;
 		}
 	}
 	void saveVideo()
 	{
+		int ErrorID = 0;
 		char name[64];
 		sprintf_s(name, "C:/CameraRecord/sucai%d.avi", m_hCamera);
-		CameraInitRecord(m_hCamera,1, name, true, 80, 60);
+		ErrorID = CameraInitRecord(m_hCamera, 1, name, true, 80, 60);
+		if (!ErrorID)
+			printf("Camera %d CameraInitRecord\n", m_hCamera);
+		else
+			printf("Camera %d CameraInitRecordError %d\n", m_hCamera, ErrorID);
 		int i = m_BufIdx;
 		do
 		{
 			CameraPushFrame(m_hCamera,m_Buffer[i].pFrameBuffer,&m_Buffer[i].pFrameHead);
-			i = i + 1 < LONTH ? i + 1 : 0;
+			i = i + 1 < VideoLenth ? i + 1 : 0;
 		} while (i != m_BufIdx);
-		CameraStopRecord(m_hCamera);
+		ErrorID = CameraStopRecord(m_hCamera);
+		if (!ErrorID)
+			printf("Camera %d CameraStopRecord\n", m_hCamera);
+		else
+			printf("Camera %d CameraStopRecordError %d\n", m_hCamera, ErrorID);
 	}
 };
 
@@ -107,7 +144,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	CameraSdkInit(1);
 	CameraEnumerateDevice(cameraInfo, &cameraNum);
-
+	printf("CameraNum = %d\n", cameraNum);
 	int length = 0;
 	char* com_num;
 	get_com_port(&com_num, length);
@@ -115,7 +152,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		printf("打开小盒子\n");
 	}
-	set_threshold(800);
+	int Threshold = 800;
+	printf("请输入声音感应灵敏度：\n");
+	scanf_s("%d", &Threshold);
+	set_threshold(Threshold);
+	printf("请输入视频帧数：\n");
+	scanf_s("%d", &VideoLenth);
 
 	Camera* pCamera1 = new Camera();
 	Camera* pCamera2 = new Camera();
@@ -166,4 +208,3 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	return 0;
 }
-
